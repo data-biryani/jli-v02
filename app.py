f6b887eafcfba547b0ac071413d604e0221ac6ab
Soumendra Dhanee <soumendra@gmail.com>
2021-01-20 09:02:25 +0530

    update

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ import pandas as pd
 
 st.sidebar.markdown("# JLI v2")
 
+st.markdown
 uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
 if uploaded_csv:
     df = pd.read_csv(uploaded_csv)
-
-st.write(df)
+    st.write(df)
+    st.write(df.describe())
