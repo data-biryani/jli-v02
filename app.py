07e7f5545f86694c448f7f20502cbee2de92f69f
Soumendra Dhanee <soumendra@gmail.com>
2021-01-20 09:23:54 +0530

    update

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from judge import Judge
 
 
 st.sidebar.markdown("# JLI v2")
@@ -7,22 +8,22 @@ mode = st.sidebar.radio(label="", options=["Upload csv", "Judges", "Judgments", 
 
 if mode=="Upload csv":
     st.markdown("## Upload judge data")
-    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
-    if uploaded_csv:
-        df = pd.read_csv(uploaded_csv)
-        st.write(df)
-        st.write(df.describe())
+    uploaded_csv_judge = st.file_uploader("Upload CSV", type="csv", key="judge")
+    if uploaded_csv_judge:
+        df_judge = pd.read_csv(uploaded_csv_judge)
+        st.write(df_judge)
+        st.write(df_judge.describe())
     
     st.markdown("## Upload judgment data")
-    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
-    if uploaded_csv:
-        df = pd.read_csv(uploaded_csv)
-        st.write(df)
-        st.write(df.describe())
+    uploaded_csv_judgment = st.file_uploader("Upload CSV", type="csv", key="judgment")
+    if uploaded_csv_judgment:
+        df_judgment = pd.read_csv(uploaded_csv_judgment)
+        st.write(df_judgment)
+        st.write(df_judgment.describe())
     
-        st.markdown("## Upload judgment text data")
-    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
-    if uploaded_csv:
-        df = pd.read_csv(uploaded_csv)
-        st.write(df)
-        st.write(df.describe())
+    st.markdown("## Upload judgment text data")
+    uploaded_csv_text = st.file_uploader("Upload CSV", type="csv", key="text")
+    if uploaded_csv_text:
+        df_text = pd.read_csv(uploaded_csv_text)
+        st.write(df_text)
+        st.write(df_text.describe())
