3acf4c3f814677e84229c1f873cc2fe5bd2abb81
Soumendra Dhanee <soumendra@gmail.com>
2021-01-20 08:25:58 +0530

    update

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
+import pandas as pd
 
 
 st.sidebar.markdown("# JLI v2")
+
+uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
+if uploaded_csv:
+    df = pd.read_csv(uploaded_csv)
+
+st.write(df)
