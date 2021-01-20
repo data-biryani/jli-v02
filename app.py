4a7a44d5dacc11e86945fe877c40a3be6240e252
Soumendra Dhanee <soumendra@gmail.com>
2021-01-20 09:18:40 +0530

    update

--- app.py
+++ app.py
@@ -3,10 +3,26 @@ import pandas as pd
 
 
 st.sidebar.markdown("# JLI v2")
+mode = st.sidebar.radio(label="", options=["Upload csv", "Judges", "Judgments", "Judgment text"], index=0,)
 
-st.markdown
-uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
-if uploaded_csv:
-    df = pd.read_csv(uploaded_csv)
-    st.write(df)
-    st.write(df.describe())
+if mode=="Upload csv":
+    st.markdown("## Upload judge data")
+    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
+    if uploaded_csv:
+        df = pd.read_csv(uploaded_csv)
+        st.write(df)
+        st.write(df.describe())
+    
+    st.markdown("## Upload judgment data")
+    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
+    if uploaded_csv:
+        df = pd.read_csv(uploaded_csv)
+        st.write(df)
+        st.write(df.describe())
+    
+        st.markdown("## Upload judgment text data")
+    uploaded_csv = st.file_uploader("Upload CSV", type="csv", accept_multiple_files=False)
+    if uploaded_csv:
+        df = pd.read_csv(uploaded_csv)
+        st.write(df)
+        st.write(df.describe())
---
+++ judge.py
@@ -0,0 +1,6 @@
+import attr
+
+@attr.s
+class Judge:
+    name: str
+    code: str
