81f3fe8c08a3862913d7f7e0fa039f2f275ec663
Soumendra Dhanee <soumendra@gmail.com>
2021-01-20 09:30:06 +0530

    update

--- app.py
+++ app.py
@@ -11,19 +11,31 @@ if mode=="Upload csv":
     uploaded_csv_judge = st.file_uploader("Upload CSV", type="csv", key="judge")
     if uploaded_csv_judge:
         df_judge = pd.read_csv(uploaded_csv_judge)
-        st.write(df_judge)
-        st.write(df_judge.describe())
+        st.write(f"Read {df_judge.shape[0]} rows and {df_judge.shape[0]} columns of data")
     
     st.markdown("## Upload judgment data")
     uploaded_csv_judgment = st.file_uploader("Upload CSV", type="csv", key="judgment")
     if uploaded_csv_judgment:
         df_judgment = pd.read_csv(uploaded_csv_judgment)
-        st.write(df_judgment)
-        st.write(df_judgment.describe())
+        st.write(f"Read {df_judgment.shape[0]} rows and {df_judgment.shape[0]} columns of data")
     
     st.markdown("## Upload judgment text data")
     uploaded_csv_text = st.file_uploader("Upload CSV", type="csv", key="text")
     if uploaded_csv_text:
         df_text = pd.read_csv(uploaded_csv_text)
+        st.write(f"Read {df_text.shape[0]} rows and {df_text.shape[0]} columns of data")
+
+if mode == "Judges":
+    if uploaded_csv_judge:
+        st.write(df_judge)
+        st.write(df_judge.describe())
+
+if mode == "Judgments":
+    if uploaded_csv_judgment:
+        st.write(df_judgment)
+        st.write(df_judgment.describe())
+
+if mode == "Judgment text":
+    if uploaded_csv_text:
         st.write(df_text)
         st.write(df_text.describe())
